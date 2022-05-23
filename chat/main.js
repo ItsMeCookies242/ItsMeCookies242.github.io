b62f465d5d5ee3dd6e82128a9ca8e15a2b51c9da
ItsMeCookies242 <97068641+ItsMeCookies242@users.noreply.github.com>
2022-05-22 22:37:08 -0400

    kinda

--- chat/main.js
+++ chat/main.js
@@ -1,14 +1,25 @@
 // tinyurl.com/subtocookies
 // eslint-disable-next-line no-undef
 const Peer = new Peer();
+// eslint-disable-next-line no-var
+var oldChat = ' ';
+// eslint-disable-next-line no-var
+var newChat = '';
+// eslint-disable-next-line space-before-function-paren
 
-function send() {
-    let msg = document.getElementById('msgSend')
-    const conn = Peer.connect('another-peers-id');
+
+// eslint-disable-next-line no-unused-vars
+function sendMsg(msg) {
+    /* const conn = Peer.connect('another-peers-id');
     // on open will be launch when you successfully connect to PeerServer
     // eslint-disable-next-line space-before-function-paren
     conn.on('open', function () {
         // here you have conn.id
-        conn.send();
-    });
+        conn.send(msg);
+    });*/
+    console.log(msg);
+    newChat = oldChat + '<br>' + msg;
+    newChat = newChat.replace('undefined', '');
+    document.getElementById('chat').innerHTML = newChat;
+    oldChat = newChat;
 }
