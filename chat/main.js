d1a4f2e4500c80d3f2961c731cce66868eff3c9e
ItsMeCookies242 <97068641+ItsMeCookies242@users.noreply.github.com>
2022-05-22 21:30:00 -0400

    idk

--- chat/main.js
+++ chat/main.js
@@ -1,1 +1,14 @@
-//tinyurl.com/subtocookies
+// tinyurl.com/subtocookies
+// eslint-disable-next-line no-undef
+const Peer = new Peer();
+
+function send() {
+    let msg = document.getElementById('msgSend')
+    const conn = Peer.connect('another-peers-id');
+    // on open will be launch when you successfully connect to PeerServer
+    // eslint-disable-next-line space-before-function-paren
+    conn.on('open', function () {
+        // here you have conn.id
+        conn.send();
+    });
+}
